0f2c110794b8f7f5fc80c408f9a804e6f7158065
Devesh Shakya <devesh.shakya@redflex.com>
2021-02-14 11:36:04 +0530

    removed cors

--- server.js
+++ server.js
@@ -1,13 +1,19 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const cors = require('cors');
+// const cors = require('cors');
 const knex = require('knex');
 
 const upload = require('./handlers/upload');
 
 const API = express();
 
-API.use(cors());
+API.use(function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
+    res.setHeader('Access-Control-Allow-Headers', 'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, X-Access-Token, XKey, Authorization');
+    next();
+});
+
 API.use(bodyParser.json());
 
 const database = knex({
